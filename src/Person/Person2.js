6c3a0120162afcfb89662b03a64a7e69a0b380f8
Rahul Srivastav <36945601+Rahul-111@users.noreply.github.com>
2020-04-04 20:14:41 +0530

    Create Person2.js

---
+++ src/Person/Person2.js
@@ -0,0 +1,11 @@
+import React from "react"
+function Person(props){
+    return(
+        <div className="Person">
+            <p onClick={props.click}>I am {props.name} and {props.age}</p>
+            <input type="text" onChange = {props.changed} value = {props.name}/>
+        </div>
+    )
+}
+
+export default Person;
